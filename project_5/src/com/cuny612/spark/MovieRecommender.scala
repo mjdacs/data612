3646402b270f3848e7f752ddab66bcffe689a8b9
Mike James <mdacampora@gmail.com>
2019-07-06 13:58:05 -0400

    .scala updated

--- project_5/src/com/cuny612/spark/MovieRecommender.scala
+++ project_5/src/com/cuny612/spark/MovieRecommender.scala
@@ -3,36 +3,113 @@ package com.cuny612.spark
 
 import org.apache.spark._
 import org.apache.spark.SparkContext._
-import org.apache.log4j._
 import org.apache.spark.sql.SparkSession
-import sqlContext.implicits._
-import org.apache.spark.mllib.recommendation.ALS
-import org.apache.spark.mllib.recommendation.MatrixFactorizationModel
 import org.apache.spark.mllib.recommendation.Rating
-import scala.Tuple2
-import org.apache.spark.rdd.RDD
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.recommendation.ALS
+
 
 
 object MovieRecommender {
   def main(args: Array[String]) {
     
-    println("testing object")
-    
-    Logger.getLogger("org").setLevel(Level.ERROR)
-    
-    val sc = new SparkContext("local[*]", "MovieRecommender")
-    
-    val lines = sc.textFile("/Users/deborahgemellaro/Programming/612/project_5/ml-latest-small/ratings.csv")
-    
-    val ratings = lines.map(x => x.toString().split(",")(2))
-    
-    val results = ratings.countByValue()
-    
-    val sortedResults = results.toSeq.sortBy(_._1)
-    lines.take(6).foreach(println)
-   
+    // Initiate session
+    val spark = SparkSession
+    .builder
+    .appName("MovieLens Recommendation")
+    .master("local[*]")
+    .getOrCreate()
+      
+    // Path to data
+    val ratingsFile = "/Users/deborahgemellaro/Programming/612/project_5/ratings.csv"
+    val moviesFile = "/Users/deborahgemellaro/Programming/612/project_5/movies.csv"
   
-    val df = lines.toDF()
     
-  }
+    // Load data as Spark DataFrame  
+    val df1 = spark.read.format("csv")
+      .option("header", true)
+      .load(ratingsFile)
+           
+    val df2 = spark.read.format("csv")
+      .option("header", true)
+      .load(moviesFile)
+      
+    // Select appropriate columns
+    val ratingsDF = df1.select(df1.col("userId"), 
+                           df1.col("movieId"), 
+                           df1.col("rating"), 
+                           df1.col("timestamp"))
+ 
+    val moviesDF = df2.select(df2.col("movieId"), 
+                          df2.col("title"),
+                          df2.col("genres"))
+                            
+    
+    // Create your training and test data                      
+    val splits = ratingsDF.randomSplit(Array(0.75, 0.25), seed = 123L)
+    val (trainingData, testData) = (splits(0), splits(1))
+  
+    // Print the counts
+    println("Counts for -- Training Data: " + trainingData.count() + ", Test Data: " + testData.count())                 
+    
+    // spark implicits needed here for serializing 
+    import spark.implicits._
+    
+    // Convert training data from strings to numerics 
+    val trainingSet = trainingData.map(row => {
+      val userId = row.getString(0)
+      val movieId = row.getString(1)
+      val ratings = row.getString(2)
+      Rating(userId.toInt, movieId.toInt, ratings.toDouble)
+  })
+    
+    // Convert test data from strings to numerics
+    val testSet = testData.map(row => {
+      val userId = row.getString(0)
+      val movieId = row.getString(1)
+      val ratings = row.getString(2)
+      Rating(userId.toInt, movieId.toInt, ratings.toDouble)
+  })
+    
+    // Create model
+    val als = new ALS()
+      .setMaxIter(5)
+      .setRegParam(0.01)
+      .setUserCol("user")
+      .setItemCol("product")
+      .setRatingCol("rating")
+  
+    // Fit model
+    val model = als.fit(trainingSet)
+    
+    // Predict on the testSet
+    val predictions = model.transform(testSet).na.drop()
+    
+    // Evaluate model
+    val evaluatorRMSE = new RegressionEvaluator()
+        .setMetricName("rmse")
+        .setLabelCol("rating")
+        .setPredictionCol("prediction")
+    
+    val evaluatorMSE = new RegressionEvaluator()
+        .setMetricName("mse")
+        .setLabelCol("rating")
+        .setPredictionCol("prediction")
+    
+    val evaluatorMAE = new RegressionEvaluator()
+        .setMetricName("mae")
+        .setLabelCol("rating")
+        .setPredictionCol("prediction")
+    
+    val rmse = evaluatorRMSE.evaluate(predictions)
+    val mse = evaluatorMSE.evaluate(predictions)
+    val mae = evaluatorMAE.evaluate(predictions)
+    
+    println(f"RMSE = $rmse")
+    println(f"MSE = $mse")
+    println(f"MAE = $mae")
+  
+  spark.stop()
+
+  } 
 }
